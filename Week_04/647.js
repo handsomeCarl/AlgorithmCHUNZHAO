48cacfce4cb468227c020190428ccc11d777ea41
wangwei <wangwei.d@nequal.com>
2021-02-21 19:31:44 +0800

    week_04  homework
    Signed-off-by: wangwei <wangwei.d@nequal.com>

---
+++ Week_04/647.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countSubstrings = function(s) {
+  const len = s.length;
+   let count = 0;
+   const dp = new Array(len);
+ 
+   for (let j = 0; j < len; j++) {
+     for (let i = 0; i <= j; i++) {
+       if (s[i] == s[j] && (j - i <= 1 || dp[i + 1])) {
+         dp[i] = true;
+         count++;
+       } else {
+         dp[i] = false;
+       }
+     }
+   }
+   return count;
+ 
+ };
